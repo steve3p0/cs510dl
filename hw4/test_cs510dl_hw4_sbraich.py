20439faa5757bb868090ec4f8f942d56f406536b
Steve Braich <33675736+steve3p0@users.noreply.github.com>
2021-03-09 00:31:24 -0800

    initial

---
+++ hw4/test_cs510dl_hw4_sbraich.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+
+from cs510dl_hw4_sbraich import Cluster
+
+class TestCluster(TestCase):
+
+    def test_bind_data(self):
+        self.fail()
+
+    def test_bench_k_means(self):
+        self.fail()
+
+    def test_evaluate(self):
+        self.fail()
+
+    def test_visualize(self):
+        self.fail()
+
+    def test_part1(self):
+        cluster = Cluster()
+        cluster.evaluate()
+        cluster.visualize()
